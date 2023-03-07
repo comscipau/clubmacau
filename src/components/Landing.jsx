ac2ae55a66a8fad78225bd45176c66fb9187812f
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-03-07 22:26:50 +0800

    logo no mt

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -50,7 +50,7 @@ const Landing = () => {
       <motion.img
         src={MACAU}
         alt="MACAU"
-        className="mt-[2.875rem] lg:mt-[5.3125rem]"
+        className="lg:mt-[5.3125rem]"
         variants={fadeInToUp}
         initial="hidden"
         whileInView="visible"
