63c746205c1a9eb86a69311b8714ad5171861ab8
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-03-08 04:23:29 +0800

    b4

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -68,7 +68,7 @@ const Landing = () => {
         Click here for more information
       </motion.a>
       <motion.p
-        className="font-spacegrotesk font-light text-xs absolute mx-0 bottom-20 md:bottom-40 lg:hidden"
+        className="font-spacegrotesk font-light text-xs absolute mx-0 bottom-40 md:bottom-40 lg:hidden"
         variants={fadeInToUp}
         initial="hidden"
         whileInView="visible"
