489478849d55fce0cf8345081d0bc16fb06b41f4
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-03-07 18:00:18 +0800

    mt to pt

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,7 +14,7 @@ const Landing = () => {
     } else {
       // Render a countdown
       return (
-        <p className="mt-8 text-4xl font-bold lg:text-9xl lg:mt-16">
+        <p className="pt-8 text-4xl font-bold lg:text-9xl lg:pt-16">
           {days}
           <span className="ml-2 text-2xl lg:text-7xl lg:ml-4">D</span> {hours}
           <span className="ml-2 text-2xl lg:text-7xl lg:ml-4">H</span> {minutes}
@@ -26,20 +26,20 @@ const Landing = () => {
   };
 
   return (
-    <div className="flex flex-col items-center w-full h-screen text-white gradientBG">
+    <div className="flex flex-col items-center w-full text-white gradientBG">
       <img
         src={MACAU}
         alt="MACAU"
-        className="mt-[3.875rem] lg:mt-[5.3125rem]"
+        className="pt-[3.875rem] lg:pt-[5.3125rem]"
       />
       <Countdown date="2023-06-01T00:00:00" renderer={renderer} />
       <a
         href="#"
-        className="underline font-spacegrotesk font-light text-base lg:text-2xl mt-8 lg:mt-[3.75rem]"
+        className="underline font-spacegrotesk font-light text-base lg:text-2xl pt-8 lg:pt-[3.75rem]"
       >
         Click here for more information
       </a>
-      <p className="mt-[11.9375rem] lg:mt-[13.1875rem] font-spacegrotesk font-light text-xs lg:text-2xl pb-[17px] lg:pb-[2.75rem]">
+      <p className="pt-[11.9375rem] lg:pt-[13.1875rem] font-spacegrotesk font-light text-xs lg:text-2xl pb-[17px] lg:pb-[2.75rem]">
         Copyright {new Date().getFullYear()} ClubMacau | All Rights Reserved.
       </p>
     </div>
