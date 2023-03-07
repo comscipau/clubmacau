c67875b638a8521b1892fe7d73275438c76bdeaf
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-03-07 18:48:18 +0800

    animate

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import Countdown from "react-countdown";
 import MACAU from "../assets/macau_lg.png";
+import { motion } from "framer-motion";
 
 const Landing = () => {
+  const fadeInToUp = {
+    hidden: {
+      y: 10,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut",
+      },
+    },
+  };
+
   // Random component
   const Completionist = () => <span>You are good to go!</span>;
 
@@ -14,35 +30,55 @@ const Landing = () => {
     } else {
       // Render a countdown
       return (
-        <p className="pt-8 text-4xl font-bold lg:text-9xl lg:pt-16">
+        <motion.p
+          className="mt-8 text-4xl font-bold lg:text-9xl lg:mt-16"
+          variants={fadeInToUp}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+        >
           {days}
           <span className="ml-2 text-2xl lg:text-7xl lg:ml-4">D</span> {hours}
           <span className="ml-2 text-2xl lg:text-7xl lg:ml-4">H</span> {minutes}
           <span className="ml-2 text-2xl lg:text-7xl lg:ml-4">M</span> {seconds}
           <span className="ml-2 text-2xl lg:text-7xl lg:ml-4">S</span>
-        </p>
+        </motion.p>
       );
     }
   };
 
   return (
     <div className="flex flex-col items-center text-white gradientBG">
-      <img
+      <motion.img
         src={MACAU}
         alt="MACAU"
-        // className="pt-[3.875rem] lg:pt-[5.3125rem]"
-        className="pt-[4.3125rem] lg:pt-[5.3125rem]"
+        // className="mt-[3.875rem] lg:mt-[5.3125rem]"
+        className="mt-[4.3125rem] lg:mt-[5.3125rem]"
+        variants={fadeInToUp}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
       />
       <Countdown date="2023-06-01T00:00:00" renderer={renderer} />
-      <a
+      <motion.a
         href="#"
-        className="underline font-spacegrotesk font-light text-base lg:text-2xl pt-8 lg:pt-[3.75rem]"
+        className="underline font-spacegrotesk font-light text-base lg:text-2xl mt-8 lg:mt-[3.75rem]"
+        variants={fadeInToUp}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
       >
         Click here for more information
-      </a>
-      <p className="pt-[14rem] font-spacegrotesk font-light text-xs lg:text-2xl pb-[17px] lg:pb-[2.75rem]">
+      </motion.a>
+      <motion.p
+        className="mt-[14rem] font-spacegrotesk font-light text-xs lg:text-2xl mb-[17px] lg:mb-[2.75rem]"
+        variants={fadeInToUp}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
         Copyright {new Date().getFullYear()} ClubMacau | All Rights Reserved.
-      </p>
+      </motion.p>
     </div>
   );
 };
