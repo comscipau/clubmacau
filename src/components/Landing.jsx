6b755147ffd28aa4f553a40e2f14cc4570421ccf
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-03-08 16:14:17 +0800

    woi

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,7 @@
 import Countdown from "react-countdown";
 import MACAU from "../assets/macau_lg.png";
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Landing = () => {
   const fadeInToUp = {
@@ -45,8 +45,27 @@ const Landing = () => {
     }
   };
 
+  var myAdjBottom = "bottom-20";
+  const [windowHeight, setWindowHeight] = useState(window.screen.height);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowHeight(window.screen.height);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
+
+  // if (windowHeight <= 800) {
+  //   myAdjBottom = "bottom-20";
+  // } else {
+  //   myAdjBottom = "bottom-10";
+  // }
+
   return (
     <div className="flex flex-col items-center text-white gradientBG relative h-screen">
+      <p>{windowHeight}</p>
       <motion.img
         src={MACAU}
         alt="MACAU"
@@ -69,7 +88,7 @@ const Landing = () => {
       </motion.a>
       <motion.p
         // className="font-spacegrotesk font-light text-xs absolute mx-0 top-40 lg:hidden"
-        className={`font-spacegrotesk font-light text-xs lg:text-2xl absolute mx-0 bottom-20`}
+        className={`font-spacegrotesk font-light text-xs lg:text-2xl absolute mx-0 ${myAdjBottom}`}
         variants={fadeInToUp}
         initial="hidden"
         whileInView="visible"
